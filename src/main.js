b1619373679797088a77f3d9e94d42ec3dd5b937
Amir Esmaeili <amiresm17@gmail.com>
2021-05-02 23:45:16 +0430

    server url changed

--- src/main.js
+++ src/main.js
@@ -6,8 +6,7 @@ import vuetify from './plugins/vuetify';
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://anatidaephobia.pythonanywhere.com/'
-// axios.defaults.baseURL = 'http://api.stage.swan-app.ir'
-axios.defaults.baseURL = "http://localhost:9090"
+axios.defaults.baseURL = 'http://api.stage.swan-app.ir'
 const token = localStorage.getItem('token')
 if (token) {
   // Set the token for auth header

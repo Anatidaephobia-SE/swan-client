acff15902734751243b6de2c2a206ec8d7738420
Amir Esmaeili <amiresm17@gmail.com>
2021-05-07 19:22:05 +0430

    adding new card

--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -123,6 +123,7 @@ export default {
       return [
         {label: 'Posts', link: 'posts', icon: 'mdi-note'},
         {label: 'Create new post', link: `compose`, icon: 'mdi-note-plus'},
+        {label: 'Ideas', link: 'ideas', icon: 'mdi-clipboard-list-outline'},
         {label: 'Settings', link: `settings`, icon: 'mdi-cog'},
       ]
     },

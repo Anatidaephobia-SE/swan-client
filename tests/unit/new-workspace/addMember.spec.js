104a9de8d55445208a757a67d91dfc93149322ac
Amir Esmaeili <amiresm17@gmail.com>
2021-05-17 21:31:51 +0430

    add member

---
+++ tests/unit/new-workspace/addMember.spec.js
@@ -0,0 +1,42 @@
+import {createLocalVue, mount} from "@vue/test-utils";
+import Vuetify from 'vuetify';
+import Vuex from "vuex"
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import AddMember from "@/components/new-workspace/AddMember"
+
+Vue.use(Vuetify)
+const localVue = createLocalVue()
+localVue.use(Vuetify)
+localVue.use(Vuex)
+localVue.use(VueRouter)
+
+
+describe("Testing adding members to workspace", () => {
+    let store, actions, wrapper
+    beforeEach(() => {
+        actions = {
+            addUserToWorkspace: jest.fn()
+        }
+        store = new Vuex.Store({
+            actions
+        })
+        const vuetify = new Vuetify({})
+        const router = new VueRouter({})
+        wrapper = mount(AddMember, {
+            localVue,
+            store,
+            router,
+            vuetify,
+            propsData: {
+                id: 1
+            }
+        })
+    })
+    it("Should call add function", () => {
+        const form = wrapper.findComponent({ref: 'userForm'})
+        expect(form.exists()).toBeTruthy()
+        form.trigger('submit.prevent')
+        expect(actions.addUserToWorkspace).toHaveBeenCalled()
+    })
+})

7f970b249e3ff7ec0d3df53a1c79d2a8fbe7c492
Amir Esmaeili <amiresm17@gmail.com>
2021-05-31 11:03:11 +0430

    landing page - top and 2nd row

---
+++ src/views/Landing.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <introduction />
+      <SocialMedia />
+  </div>
+</template>
+
+<script>
+import Introduction from "@/components/landing/Introduction.vue"
+import SocialMedia from "@/components/landing/SocialMedia.vue"
+export default {
+    name: 'Landing',
+    components: {Introduction, SocialMedia},
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style>
+
+</style>

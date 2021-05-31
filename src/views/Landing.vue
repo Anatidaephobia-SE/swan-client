ada7553ab47638a487fbd2ab1aa6907954dd42e3
Amir Esmaeili <amiresm17@gmail.com>
2021-05-31 13:03:07 +0430

    landing beta

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -4,6 +4,8 @@
       <SocialMedia />
       <Ideas />
       <Calendar />
+      <Email />
+      <Recommend />
   </div>
 </template>
 
@@ -12,10 +14,13 @@ import Introduction from "@/components/landing/Introduction.vue"
 import SocialMedia from "@/components/landing/SocialMedia.vue"
 import Ideas from "@/components/landing/Ideas.vue"
 import Calendar from "@/components/landing/Calendar.vue"
+import Email from '../components/landing/Email.vue'
+import Recommend from '../components/landing/Recommend.vue'
+
 
 export default {
     name: 'Landing',
-    components: {Introduction, SocialMedia, Ideas, Calendar},
+    components: {Introduction, SocialMedia, Ideas, Calendar, Email, Recommend},
     data() {
         return {}
     }

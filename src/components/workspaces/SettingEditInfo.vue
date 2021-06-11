b420014fab4e912b6ef585394d1b705b1b84f540
Amir Esmaeili <amiresm17@gmail.com>
2021-06-11 12:39:33 +0430

    added loader for edit info

--- src/components/workspaces/SettingEditInfo.vue
+++ src/components/workspaces/SettingEditInfo.vue
@@ -2,75 +2,81 @@
   <v-container>
     <v-card-title>
       <v-icon class="mr-2">mdi-information</v-icon>
-      Info</v-card-title>
-      <v-row>
-        <v-col cols="12" md="3">
+      Info
+    </v-card-title>
+    <v-row v-if="!pageLoading">
+      <v-col cols="12" md="3">
 
-          <v-menu v-model="imageMenu"
-                  :disabled="!editMode"
-                  transition="slide-y-transition"
-                  offset-y
-                  :rounded="'b-xl'"
-                  bottom>
-            <template v-slot:activator="{on, attrs}">
-              <v-avatar rounded v-bind="attrs" v-on="on" class="user-avatar" size="200">
-                <v-icon v-show="editMode && !info.logo" size="50">mdi-camera-plus</v-icon>
-                <v-icon v-show="!editMode && !info.logo" size="50">mdi-account</v-icon>
-                <v-img v-if="info.logo" :src="getImgUrl"></v-img>
-              </v-avatar>
-            </template>
+        <v-menu v-model="imageMenu"
+                :disabled="!editMode"
+                transition="slide-y-transition"
+                offset-y
+                :rounded="'b-xl'"
+                bottom>
+          <template v-slot:activator="{on, attrs}">
+            <v-avatar rounded v-bind="attrs" v-on="on" class="user-avatar" size="200">
+              <v-icon v-show="editMode && !info.logo" size="50">mdi-camera-plus</v-icon>
+              <v-icon v-show="!editMode && !info.logo" size="50">mdi-account</v-icon>
+              <v-img v-if="info.logo" :src="getImgUrl"></v-img>
+            </v-avatar>
+          </template>
 
-            <v-file-input ref="imageInput" v-show="false" v-model="info.logo" accpet="image/*"></v-file-input>
+          <v-file-input ref="imageInput" v-show="false" v-model="info.logo" accpet="image/*"></v-file-input>
 
-            <v-list>
-              <v-list-item @click="info.logo = ''" v-if="info.logo">
-                <v-list-item-title>Remove image</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="$refs.imageInput.$refs.input.click()">
-                <v-list-item-title>Update image</v-list-item-title>
-              </v-list-item>
+          <v-list>
+            <v-list-item @click="info.logo = ''" v-if="info.logo">
+              <v-list-item-title>Remove image</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$refs.imageInput.$refs.input.click()">
+              <v-list-item-title>Update image</v-list-item-title>
+            </v-list-item>
 
 
-            </v-list>
+          </v-list>
 
-          </v-menu>
-        </v-col>
-        <v-col cols="12" md="4">
-          <v-text-field
-              v-model="info.name"
-              label="Workspace Name"
-              :disabled="!editMode"
-              filled>
-          </v-text-field>
-          <v-text-field
-              :disabled="!editMode"
-              v-model="info.url"
-              filled label="Workspace URL">
-          </v-text-field>
-        </v-col>
-      </v-row>
-      <v-card-actions v-if="canEdit">
-        <v-spacer>
-        </v-spacer>
-        <v-btn ref="editBtn" v-if="!editMode" @click="editMode = true" depressed color="primary">Edit</v-btn>
-        <v-btn ref="discardBtn" outlined @click="discardChanges" v-if="editMode" color="error">Discard</v-btn>
-        <v-btn ref="saveBtn" depressed @click="saveChanges" :loading="loading" v-if="editMode" color="primary">Save</v-btn>
-      </v-card-actions>
+        </v-menu>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-text-field
+            v-model="info.name"
+            label="Workspace Name"
+            :disabled="!editMode"
+            filled>
+        </v-text-field>
+        <v-text-field
+            :disabled="!editMode"
+            v-model="info.url"
+            filled label="Workspace URL">
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <SettingEditInfoLoader v-else />
+    <v-card-actions v-if="canEdit">
+      <v-spacer>
+      </v-spacer>
+      <v-btn ref="editBtn" v-if="!editMode" @click="editMode = true" depressed color="primary">Edit</v-btn>
+      <v-btn ref="discardBtn" outlined @click="discardChanges" v-if="editMode" color="error">Discard</v-btn>
+      <v-btn ref="saveBtn" depressed @click="saveChanges" :loading="loading" v-if="editMode" color="primary">Save
+      </v-btn>
+    </v-card-actions>
   </v-container>
 </template>
 
 <script>
 import axios from "axios";
+import SettingEditInfoLoader from "@/components/workspaces/SettingEditInfoLoader";
 
 export default {
   name: "SettingEditInfo",
+  components: {SettingEditInfoLoader},
   data() {
     return {
       info: {},
       editMode: false,
       imageMenu: false,
       loading: false,
-      canEdit: false
+      canEdit: false,
+      pageLoading: false
     }
   },
   computed: {
@@ -78,9 +84,9 @@ export default {
       return this.$route.params.workspace
     },
     getImgUrl: function () {
-      if (typeof(this.info.logo) === 'string') {
+      if (typeof (this.info.logo) === 'string') {
         return axios.defaults.baseURL + this.info.logo;
-      } else if (typeof(this.info.logo) === 'object') {
+      } else if (typeof (this.info.logo) === 'object') {
         return URL.createObjectURL(this.info.logo)
       }
       return ''
@@ -91,12 +97,14 @@ export default {
   },
   methods: {
     getInfo: function () {
+      this.pageLoading = true
       this.$store.dispatch('getWorkspaceInfo', this.getWorkspaceId).then(resp => {
         this.info = resp.data.team;
         this.canEdit = resp.data.can_edit
+        this.pageLoading = false
       }).catch(err => {
         const message = err.response.data.error;
-        this.$store.dispatch('showMessage', {message , color: 'error'});
+        this.$store.dispatch('showMessage', {message, color: 'error'});
       });
     },
     discardChanges: function () {
@@ -114,12 +122,12 @@ export default {
       }
       this.$store.dispatch('editWorkspaceInfo', body).then(() => {
         const message = `Workspace info updated!`;
-        this.$store.dispatch('showMessage', {message , color: 'success'});
+        this.$store.dispatch('showMessage', {message, color: 'success'});
         this.editMode = false;
         this.getInfo();
       }).catch(err => {
         const message = err.response.data.error;
-        this.$store.dispatch('showMessage', {message , color: 'error'});
+        this.$store.dispatch('showMessage', {message, color: 'error'});
       }).finally(() => this.loading = false);
     },
   }
---
+++ src/components/workspaces/SettingEditInfoLoader.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row>
+    <v-col cols="12" md="3">
+      <v-skeleton-loader
+          class="mx-auto"
+          max-width="200"
+          type="image"
+      ></v-skeleton-loader>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-skeleton-loader
+          type="list-item">
+      </v-skeleton-loader>
+
+      <v-skeleton-loader
+          type="list-item">
+      </v-skeleton-loader>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "SettingEditInfoLoader"
+}
+</script>
+
+<style scoped>
+
+</style>

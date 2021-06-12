d06615e8029a4f6f50ee0b3419e8e9afa5ce1130
Amir Esmaeili <amiresm17@gmail.com>
2021-06-12 20:34:13 +0430

    loader added for posts

---
+++ src/components/post/PostViewLoader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="loader">
+      <v-skeleton-loader
+          class="text-center d-flex justify-center"
+          type="heading"
+      ></v-skeleton-loader>
+    <v-skeleton-loader
+        class="justify-center"
+        type="list-item-two-line"
+    ></v-skeleton-loader>
+    <v-skeleton-loader
+        class="justify-center"
+        type="paragraph"
+    ></v-skeleton-loader>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "PostViewLoader"
+}
+</script>
+
+<style lang="scss" scoped>
+.loader {
+  max-width: 600px;
+  margin: auto;
+}
+</style>
